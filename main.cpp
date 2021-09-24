4a13e2a7fa018acda3e4a6176bf9ce9545649742
chaconcastillojoel <joel.chacon@cimat.mx>
2021-09-24 18:48:25 -0500

    ..

--- main.cpp
+++ main.cpp
@@ -136,6 +136,32 @@ vector<vector<double> > bnp3D(vector<vector<double> > inpoints, vector<double> &
    }
    return outpoints;
 }
+vector<vector<double> > bnp2D(vector<vector<double> > inpoints, vector<double> &reference, int S){
+   vector<vector<double> > outpoints;
+   map<double, double> front2D;
+   double inf = std::numeric_limits<double>::max();//inf can be more costly to work with!
+   front2D[-inf]=0;
+   front2D[0]=-inf;
+   for(int t = 0; t < S; t++){
+	pair<double, int> maxctr(-1.0, -1);
+	for(int i = 0; i < inpoints.size(); i++){
+	   auto point = inpoints[i];
+	   for(int j = 0; j < point.size(); j++) point[j]-=reference[j];
+	   auto right=front2D.lower_bound(point[0]);
+	   auto left=right;
+	   left--;
+	   if(left->second < point[1])continue;
+	   double area = (right->second- point[1]) * (left->first - point[0]);
+	   cout << area<<endl;
+	   maxctr= max(maxctr, make_pair(area, i));
+          }
+        front2D[inpoints[maxctr.second][0]-reference[0]] =inpoints[maxctr.second][1]-reference[1];
+        outpoints.push_back(inpoints[maxctr.second]); 
+        iter_swap(inpoints.begin()+maxctr.second, inpoints.end()-1);
+        inpoints.pop_back();
+   }
+   return outpoints;
+}
 vector<vector<double> > lala(vector<vector<double> > &inpoints, vector<double> &reference, int S){
    vector<vector<double> > outpoints;
    set<pair<double, pair<double, double > > >points;
@@ -147,18 +173,21 @@ vector<vector<double> > lala(vector<vector<double> > &inpoints, vector<double> &
    return outpoints;
 }
 ////////////////////////////////////////////////////////////7
-int nobj=3, N;
 vector<vector<double> >newpoints(int n, int m){
      srand(1);
      vector<vector<double> > points(n, vector<double>(m) ) ;
      for(int i = 0; i < n; i++){
    	  double x1= i/(double)n;
    	  double x2=rand()/(double)RAND_MAX;
-   //	  points[i][0]=x1;
-   //	  points[i][1]=1.0-pow(x1, 1.5);
+	  if(m==2){
+   	  points[i][0]=x1;
+   	  points[i][1]=1.0-pow(x1, 0.2);
+   	  }
+	  else{
             points[i][0] = cos(M_PI*x1*0.5)*cos(M_PI*x2*0.5);
             points[i][1] = cos(M_PI*x1*0.5)*sin(M_PI*x2*0.5);
             points[i][2] = sin(M_PI*x1*0.5);
+	  }
 //     points[i][0] = x1*x2;
 //     points[i][1] = x1*(1.0-x2);
 //     points[i][2] = (1.0-x1);
@@ -178,17 +207,15 @@ using std::chrono::high_resolution_clock;
     using std::chrono::duration_cast;
     using std::chrono::duration;
     using std::chrono::milliseconds;
-   auto a=newpoints(1000, 3);
-//   cout << a <<endl;
-   vector<double> ref(3, 10);
-   cout << a <<endl;
+   auto a=newpoints(101, 2);
+   vector<double> ref(2, 1.1);
 //  cout << hv3D(a,ref);
-//  exit(0);
   auto t1 = high_resolution_clock::now();
-//  for(int i = 0; i < 250; i++){
-//   auto b=bnp3D(a, ref, 100);
-//   cout << b<<endl;
- // }
+  for(int i = 0; i < 2500; i++){
+   //auto b=bnp3D(a, ref, 100);
+   auto b=bnp2D(a, ref, 100);
+ //  cout << b<<endl;
+  }
     auto t2 = high_resolution_clock::now();
 
     auto ms_int = duration_cast<milliseconds>(t2 - t1);
